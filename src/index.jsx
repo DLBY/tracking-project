8bbf388f610e738e59dbeeac11d595cfd8c8603d
David <david.lobry96@gmail.com>
2021-02-03 09:31:43 +0100

    init

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.scss';
+
+const App = () => {
+  <div className="container">
+    <h1>Welcome on Tracking Project</h1>
+  </div>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
